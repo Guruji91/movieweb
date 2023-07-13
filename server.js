0af1a24c586cae59206d4751e03a6c4c9abeb6f2
Hariom Tomar <78545781+Guruji91@users.noreply.github.com>
2023-07-13 22:59:00 +0530

    u

---
+++ server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
+import userRouter from "./Routes/UserRouter.js";
+import moviesRouter from "./Routes/MoviesRouter.js";
+import categoriesRouter from "./Routes/CategoriesRouter.js";
+import Uploadrouter from "./Controllers/UploadFile.js";
+import path from "path"
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './client/build')))
+
+// connect DB
+connectDB();
+
+// es module fix
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
+
+// Main route
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
+
+app.get("*", function(req, res)  {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// other routes
+app.use("/api/users", userRouter);
+app.use("/api/movies", moviesRouter);
+app.use("/api/categories", categoriesRouter);
+app.use("/api/upload", Uploadrouter);
+
+// error handling middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in  http://localhost/${PORT}`);
+});
