4bacecb9d6b8b4d40247029503cb4b25fcb8e8f6
Guruji <78545781+Guruji91@users.noreply.github.com>
2023-07-14 18:07:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
+import  connectDB  from "./config/db.js";
 import { errorHandler } from "./middlewares/errorMiddleware.js";
 import userRouter from "./Routes/UserRouter.js";
 import moviesRouter from "./Routes/MoviesRouter.js";
