a9689ed245709d22449e04aa07c0f9b6009c639a
Guruji <78545781+Guruji91@users.noreply.github.com>
2023-07-14 18:02:04 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,31 @@
 // connect MongoDB with mongoose
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-export const connectDB = async () => {
+// export const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.log(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+
+
+import mongoose from "mongoose";
+const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Connected");
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`
+    );
   } catch (error) {
-    console.log(`Error: ${error.message}`);
-    process.exit(1);
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
   }
 };
+
+export default connectDB;
